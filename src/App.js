d31e8fa58fcd1196d96ec512731c87a5f818b5ba
Ejique <sspirin@yandex.ru>
2021-11-09 22:05:48 +0300

    git push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HelloWorld from './HelloWorld';
+import Post from './Post';
+import posts from './posts.json'
 
 function App() {
+ const posts_list = posts.map(post=>{
+   return <Post key={post.id} title={post.title} body={post.body}/>
+ })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts_list}
+      <HelloWorld text = "Привет!" color="red"/>
+      
+
     </div>
   );
 }
---
+++ src/HelloWorld.js
@@ -0,0 +1,9 @@
+
+
+function HelloWorld(props){
+return(
+    <h1 className ={props.color}>{props.text}</h1>
+)
+}
+
+export default HelloWorld;
